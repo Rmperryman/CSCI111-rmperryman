1c3f96bfebac4ecdbcceb51fedb471a43557feaf
rmperryman <rmperryman@mavs.coloradomesa.edu>
2026-02-02 20:02:01 -0700

    lab1

---
+++ labs/lab1/ascii/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << R"(
+    |\_/|          	*****************************      (\_/)
+   / @ @ \      	*         ASCII Lab         *  	  (='.'=)
+  ( > 0 < )         *      <Robin Perryman>     * 	  (")_(")
+    >>x<<        	*       <Spring 2026>       *
+   /  O  \       	*         CSCI 111          *
+                    *****************************
+
+    
+    
+        )" << endl;
+    return 0;
+}
