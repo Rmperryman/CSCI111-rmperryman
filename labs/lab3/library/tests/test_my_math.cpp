6952951159ea98b9bbae7460ae9d79b6f41dae73
rmperryman <rmperryman@mavs.coloradomesa.edu>
2026-02-07 16:21:10 -0700

    set up labs folder

---
+++ labs/lab3/library/tests/test_my_math.cpp
@@ -0,0 +1,37 @@
+// Program to test functions defined in my_math.cpp file
+
+#include <iostream>
+#include <cmath>
+#include <cassert>
+#include "../src/my_math.h"
+using namespace std;
+
+const float EPSILON = 1e-6; // accuracy upto 6 decimal points
+
+void test_square_root()
+{
+    float num = 16.0;
+    float expected = 4.0;
+    float result = square_root(num);
+    assert(fabs(result - expected) <= EPSILON);
+}
+
+void test_log_base_2()
+{
+    float num = 1024;
+    float expected = 10;
+    float result = log_base_2(num);
+    assert(fabs(result - expected) <= EPSILON);
+}
+
+// FIXME1 - define test functions fort rest of the functions defined in my_math.cpp
+
+int main()
+{
+    cerr << "My Math Library Test Runner" << endl;
+    test_square_root();
+    test_log_base_2();
+    // FIXME2: call the rest of the test functions
+    cerr << "All test cases passed!" << endl;
+    return 0;
+}
